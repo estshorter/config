f133f3fc2fa82f3b41a933f20d493d1c941bbf72
estshorter <1430311+estshorter@users.noreply.github.com>
2021-09-05 11:53:14 +0900

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,6 @@
 # https://intellitect.com/enter-vsdevshell-powershell/
 # http://mklearning.blogspot.com/2019/12/developer-powershell-for-vs-2019x64.html
+# SkipAutomaticLocation を指定しないと勝手にcdされる
 function activate_msvc_impl {
     $installPath = &"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version 16.0 -property installationpath
     Import-Module (Join-Path $installPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll") && Enter-VsDevShell -VsInstallPath $installPath -SkipAutomaticLocation -DevCmdArguments -arch=x64
