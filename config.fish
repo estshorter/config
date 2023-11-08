ab019a07b89498213b00e341ec5cc6899d9c13e0
estshorter <1430311+estshorter@users.noreply.github.com>
2023-11-08 22:47:29 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,9 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     if command -q exa
-        alias e='exa --group-directories-first'
-        alias el='exa -hl --time-style=iso --group-directories-first'
-        alias ea='exa -hal --time-style=iso --group-directories-first'
+        alias e='eza --group-directories-first'
+        alias el='eza -hl --time-style=iso --group-directories-first'
+        alias ea='eza -hal --time-style=iso --group-directories-first'
         alias ls=e
         alias ll=el
         alias la=ea
